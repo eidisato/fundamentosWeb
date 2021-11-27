dc128f59cf7ef3c50b1b742c94e8013c711b301b
eidisato <viniciuseidi@outlook.com>
2021-11-26 22:09:27 -0300

    atualização do formulario

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -4,72 +4,54 @@ let assunto = document.querySelector('#assunto')
 let nomeOk = false
 let emailOk = false
 let assuntoOk = false
-let mapa = document.querySelector('#mapa')
+nome.style.width='100%'
+email.style.width='100%'
+assunto.style.width='100%'
 
-nome.style.width = '100%'
-email.style.width = '100%'
-
-function validaNome() {
-    let txtNome = document.querySelector('#txtNome')
-    txtNome.style.fontFamily = 'Arial Black'
-
-    if (nome.value.length < 3) {
-        txtNome.innerHTML = 'Nome Inválido'
-        txtNome.style.color = 'red'
-    } else {
-        txtNome.innerHTML = 'Nome Válido'
-        txtNome.style.color = 'green'
+function validaNome(){
+    let txtNome =document.querySelector("#txtnome")
+    if(nome.value.length < 3){
+        txtNome.innerHTML = "Nome invalido"
+        txtNome.style.color='red'
+    }
+    else{
+        txtNome.innerHTML = "Nome valido"
+        txtNome.style.color='green'
         nomeOk = true
     }
 }
 
-function validaEmail() {
-    let txtEmail = document.querySelector('#txtEmail')
-    txtEmail.style.fontFamily = 'Arial Black'
-
-    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
-        txtEmail.innerHTML = 'E-mail inválido'
-        txtEmail.style.color = 'red'
-    } else {
-        txtEmail.innerHTML = 'E-mail válido'
-        txtEmail.style.color = 'green'
+function validaEmail(){
+    let txtEmail =document.querySelector("#txtemail")
+    if(email.value.indexOf("@") == -1){
+        txtEmail.innerHTML = "E-mail Invalido"
+        txtEmail.style.color='red'
+    }
+    else{
+        txtEmail.innerHTML = "E-mail Valido"
+        txtEmail.style.color='green'
         emailOk = true
     }
 }
 
-function validaAssunto() {
-    let txtAssunto = document.querySelector('#txtAssunto')
-    txtAssunto.style.fontFamily = 'Arial Black'
-
-    if (assunto.value.length >= 100) {
-        txtAssunto.innerHTML = 'Texto é muito grande, digite no máximo 100 caracteres!'
-        txtAssunto.style.color = 'red'
-        txtAssunto.style.display = 'block'
-    } else {
-        txtAssunto.style.display = 'none'
+function validaAssunto(){
+    let txtAssunto = document.querySelector("#txtassunto")
+    if(assunto.value.length >= 500){
+        txtAssunto.innerHTML ="Envie no máximo texto com 500 caracteres"
+        txtAssunto.style.color="red"
+        txtAssunto.style.display="block"
+    }
+    else{
+        txtAssunto.style.display="none"
         assuntoOk = true
     }
 }
 
-function enviar() {
-
-    if (nomeOk == false && emailOk == true && assuntoOk == true) { alert('Nome incorreto!') }
-
-    if (nomeOk == true && emailOk == false && assuntoOk == true) { alert('E-mail incorreto!') }
-
-    if (nomeOk == true && emailOk == true && assuntoOk == false) { alert('Assunto incorreto!') }
-
-    if (nomeOk == true && emailOk == true && assuntoOk == true) { alert('Formulário enviado com sucesso!') }
-
-    else { alert('Preencha o formulário corretamente antes de enviar...') }
-}
-
-function mapaZoom() {
-    mapa.style.width = '550px'
-    mapa.style.height = '350px'
-}
-
-function mapaNormal() {
-    mapa.style.width = '400px'
-    mapa.style.height = '250px'
+function enviar(){
+    if (nomeOk == true && emailOk==true && assuntoOk==true){
+        alert("Formulario enviado com sucesso!")
+    }
+    else{
+        alert("Preencha o formulario corretamente")
+    }
 }
